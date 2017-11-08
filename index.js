6a93048ed3cec0357bbb24bc9fa0140c28e2004a
Matthew Dworkin <mdworkin21@gmail.com>
2017-11-08 19:19:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,13 +30,8 @@ function calculatesFarePrice(start, destination){
 
     if (distance < 400){
       return 0
-    } else if (distance > 400 && distance < 2000){
+    } else if (400 < distance < 2500)){
       return 25
     } else {
       return "cannot travel that far"
     }}
-
-
-
-
-
